ee742fb3866060a3159b310fe2a72eae826496ff
Hannah Ingham <hannahingham@MacBooks-MacBook-Pro.local>
2019-04-19 22:00:07 -0700

    code

---
+++ __tests__/priority-queue.js
@@ -0,0 +1,71 @@
+let { Scheduler, sayHi, saySomething, beSlow } = require("../index.js");
+
+
+let mySchedule;
+beforeEach(()=>{
+    mySchedule = new Scheduler();
+})
+
+describe("The Scheduler class", () => {
+
+    
+    it("can schedule an event", () => {
+        mySchedule.schedule(sayHi, 1);
+        let timeSet = mySchedule.queue.expectedAt >= Date.now() ? true : false;
+        expect(mySchedule.queue).not.toBeNull();
+        expect(timeSet).toBeDefined();
+    });
+    
+    it("knows how many events are queued", () => {
+      mySchedule.schedule(sayHi, 100);
+      mySchedule.schedule(sayHi, 100);
+      mySchedule.schedule(beSlow, 400);
+      expect(mySchedule.numberOfScheduled()).toEqual(3);
+    });
+  //I think this might be crashing?
+  it("correctly places a new event in the priority queueu", () => {
+    mySchedule.schedule(sayHi, 100);
+    mySchedule.schedule(beSlow, 400);
+    let func = mySchedule.queue.next.functionName;
+    expect(func).toEqual(beSlow);
+  });
+
+  it("stores in the nodes the time that the event should fire", () => {
+    mySchedule.schedule(sayHi, 700);
+    let timeout = mySchedule.queue;
+    console.log(timeout);
+    expect(timeout).toBe("number");
+  });
+
+  it("clears out events on an interval if they have already fired", () => {
+    mySchedule.schedule(sayHi, 300);
+    mySchedule.schedule(saySomething, 600);
+    mySchedule.schedule(sayHi, 2000);
+    let eventsAfter500;
+    setTimeout(()=>{
+        eventsAfter500 = mySchedule.numberOfScheduled();
+        expect(eventsAfter500).toEqual(2);
+
+    }, 500)
+    // setTimeout(()=>{
+
+    // }, )
+  });
+
+  it("cancels all events when cancel is called", () => {
+    const sayHi = jest.fn();
+    mySchedule.schedule(sayHi, 10);
+    mySchedule.cancelAll();
+    setTimeout(()=>{
+        expect(sayHi).not.toBeCalled();
+    }, 200);
+  });
+
+  it("returns the next up item when nextUp is called", () => {
+    mySchedule.schedule(saySomething, 700);
+    mySchedule.schedule(sayHi, 200);
+    let next = mySchedule.nextUp();
+    let func = mySchedule.queue;
+    expect(next).toBe(func);
+  });
+});
---
+++ index.js
@@ -0,0 +1,117 @@
+//write smaple functions
+
+function sayHi() {
+  console.log("hi");
+  return;
+}
+
+function sayHello(str) {
+  console.log("hello", str);
+  return str;
+}
+
+function beSlow() {
+  console.log("i am slow");
+  return;
+}
+
+// write node class
+class Node {
+  constructor(timeoutHandler, functionName) {
+    this.functionName = functionName;
+    this.expectedAt =
+      timeoutHandler._idleTimeout + timeoutHandler._idleStart + Date.now();
+    this.timeoutHandler = timeoutHandler;
+    this.next = null;
+  }
+}
+
+//write scheduler class
+class Scheduler {
+  constructor() {
+    this.queue = null;
+    this.intervalHandler = null;
+    this.count = 0;
+  }
+  //write a construtor with queue, count, and interval handler
+  schedule(cb, delay) {
+    let timeoutHandler = setTimeout(cb, delay);
+    this.addToPriorityQueue(new Node(timeoutHandler, cb));
+  }
+
+  //write addToPriorityQueue
+  addToPriorityQueue(node) {
+    let current = this.queue;
+    let prev = this.queue;
+    if (this.queue == null) {
+      this.queue = node;
+      this.startClearOut();
+    } else if (node.expectedAt < current.expectedAt) {
+      this.queue = node;
+      node.next = current;
+    } else {
+      while (current !== null && node.expectedAt > current.expectedAt) {
+        console.log("current, ", current);
+        prev = current;
+        current = current.next;
+      }
+      prev.next = node;
+      node.next = current;
+    }
+    this.count++;
+    return this.queue;
+  }
+
+  numberOfScheduled() {
+    console.log("count", this.count);
+    return this.count;
+  }
+
+  cancelAll() {
+    let current = this.queue;
+    while (current) {
+      clearTimeout(current.timeoutHandler);
+      current = current.next;
+    }
+  }
+
+  nextUp() {
+    return this.queue;
+  }
+
+  startClearOut() {
+    this.intervalHandler = setInterval(() => {
+      this.clearOutQueue();
+    }, 500);
+  }
+
+  clearOutQueue() {
+    console.log("count: ", this.count);
+
+    if (this.queue == null) {
+      clearInterval(this.intervalHandler);
+    } else if (this.queue.expectedAt < Date.now()) {
+      let temp = this.queue;
+      this.queue = temp.next;
+      temp.next = null;
+      this.count--;
+    }
+  }
+}
+
+//write startInvertal -- to call clearOut, pass intervalHandler
+
+//write clearOut to clear out functions already called
+
+//
+
+let mySchedule = new Scheduler();
+
+mySchedule.schedule(sayHi, 1000);
+mySchedule.schedule(sayHello, 2000);
+mySchedule.schedule(beSlow, 3000);
+mySchedule.numberOfScheduled();
+
+console.log(typeof mySchedule.queue.expectedAt);
+
+module.exports = { sayHi, sayHello, beSlow, Scheduler };
